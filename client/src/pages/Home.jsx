e31726a2cf5f3a1d5f46b4ed4732e8e4cbcc2d69
GDKobrae <djedjegboble.pro@gmail.com>
2024-09-11 23:45:48 +0200

    Refactor Home component to implement character search and pagination

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,25 @@ import { useState, useEffect, useRef } from "react";
 import { Button, Input } from "@material-tailwind/react";
 import { CardDefault } from "../components/CardDefault";
 import searchlook from "../assets/searchlook.svg";
+import { SimplePagination } from "../components/SimplePagination";
+import { setTotalPages, prevPage, nextPage } from "../redux/paginationSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 function Home() {
+  const dispatch = useDispatch();
+  const activePage = useSelector((state) => state.pagination.activePage);
+  const totalPages = useSelector((state) => state.pagination.totalPages);
+
   const inputRef = useRef(null);
 
   const [filter, setFilter] = useState("");
 
   const [characterList, setCharacterList] = useState([]);
+
+  // const [pages, setPages] = useState(1);
+
+  // const [nbTotalPages, setNbTotalPages] = useState(1);
 
   function changeFilterToAlive() {
     setFilter("Alive");
@@ -22,56 +34,59 @@ function Home() {
     setFilter("");
   }
 
-  //   function testFn(event) {
-  //     console.log(inputRef.current.value);
-  //   }
-
-  async function loadCharacterList() {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
-    const characters = await response.json();
-    setCharacterList(characters.results);
+  function testFn(event) {
+    console.log("Za WARUDO");
   }
 
-  async function loadCharacterListByName(name) {
-    const value = name.trim();
-    const response = await fetch(
-      `https://rickandmortyapi.com/api/character?name=${value}`
-    );
-    const characters = await response.json();
-    setCharacterList(characters.results);
-  }
+  async function loadCharacterList(name, page, status) {
+    let response;
+    if (name && status && page) {
+      response = await fetch(
+        `https://rickandmortyapi.com/api/character/?name=${name}&status=${status}&page=${page}`
+      );
+    } else if (name && page) {
+      response = await fetch(
+        `https://rickandmortyapi.com/api/character/?name=${name}&page=${page}`
+      );
+    } else if (page && status) {
+      response = await fetch(
+        `https://rickandmortyapi.com/api/character/?status=${status}&page=${page}`
+      );
+    } else if (page) {
+      response = await fetch(
+        `https://rickandmortyapi.com/api/character/?page=${page}`
+      );
+    }
 
-  async function loadCharacterListByPage(page) {
-    const value = page;
-    const response = await fetch(
-      `https://rickandmortyapi.com/api/?page=${page}`
-    );
     const characters = await response.json();
+    if (characters.info) {
+      dispatch(setTotalPages(characters.info.pages));
+    }
     setCharacterList(characters.results);
   }
 
   function inputOnKeyDown(event) {
     if (event.key == "Enter" && event.target.value) {
       const searchValue = event.target.value.trim();
-      loadCharacterListByName(searchValue);
+      loadCharacterList(searchValue, activePage, filter);
     }
   }
 
-  function onSearchClick(event) {
+  function onSearchClick() {
     if (inputRef.current.value) {
       const searchValue = inputRef.current.value;
-      loadCharacterListByName(searchValue);
+      loadCharacterList(searchValue, activePage, filter);
     }
   }
 
   useEffect(() => {
-    loadCharacterList();
-  }, []);
+    loadCharacterList(inputRef.current.value, activePage, filter);
+  }, [filter, dispatch, activePage, totalPages]);
 
   return (
-    <div>
-      <div className="flex m-12 justify-between mb-24">
-        <div className="flex gap-4">
+    <div className="flex flex-col mb-12 gap-12 min-w-[360px]">
+      <div className="flex flex-col-reverse m-12 gap-12 lg:flex-row lg:justify-between mb-24">
+        <div className="flex gap-4 self-center">
           <Button
             variant={filter != "Alive" ? "outlined" : "filled"}
             size="sm"
@@ -88,7 +103,7 @@ function Home() {
           </Button>
         </div>
 
-        <div>
+        <div className="min-w-60 self-center sm:min-w-96">
           <Input
             label="Search"
             variant="standard"
@@ -99,7 +114,7 @@ function Home() {
                 src={searchlook}
                 alt="Loupe"
                 className="m-0 p-0 cursor-pointer"
-                onClick={(event) => onSearchClick(event)}
+                onClick={() => onSearchClick()}
               />
             }
           />
@@ -114,17 +129,20 @@ function Home() {
                 key={character.name + index}
                 imgSrc={character.image}
                 characterName={character.name}
-                gender={character.gender}
-                species={character.species}
-                origin={character.origin.name}
-                location={character.location.name}
-                btnText={"See More"}
+                status={character.status}
+                btnText={
+                  <Link to={`/character/${character.id}`}>See More</Link>
+                }
               />
             );
           })
         ) : (
           <h1>No character found</h1>
         )}
+      </div>
+
+      <div className="flex justify-center">
+        {characterList ? <SimplePagination /> : " "}
       </div>
     </div>
   );
